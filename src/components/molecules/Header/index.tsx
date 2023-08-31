d00b82faa6ecc41859555c79a6cc57c6b49b37eb
TonySilva7 <tony.deve7oper@gmail.com>
2023-08-31 20:48:22 -0300

    feat: create Header component

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -1,11 +1,29 @@
+import { ATM } from '@APP/components';
+import { Grip } from 'lucide-react';
 import { ComponentProps } from 'react';
 
 type HeaderProps = ComponentProps<'div'>;
 
-function Header({ title = 'Header :)' }: HeaderProps) {
+function Header({ ...props }: HeaderProps) {
   return (
-    <div>
-      <h1> Hello {title}</h1>
+    <div
+      className=" flex h-14 w-full items-center justify-between border-b px-4"
+      {...props}
+    >
+      <span className="flex gap-3">
+        <ATM.Text variant="bold"> Agile Content </ATM.Text>
+        <ATM.Text> Frontend test </ATM.Text>
+      </span>
+      <div className="flex items-center gap-3 text-zinc-800">
+        <button>
+          <Grip className="font-medium text-zinc-600" />
+        </button>
+        <img
+          src="https://github.com/TonySilva7.png"
+          alt="imagem perfil do usuário"
+          className="h-8 w-8 rounded-full"
+        />
+      </div>
     </div>
   );
 }
